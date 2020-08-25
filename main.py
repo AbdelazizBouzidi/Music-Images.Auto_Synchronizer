2b15022f945dcffe7777dc7e8a2eb67800fae166
BOUZIDI Abdelaziz <42507657+AbdelazizBouzidi@users.noreply.github.com>
2020-08-25 20:13:58 +0100

    Update and rename main .py to main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from beatMaker import beat_gen
 img = ["0.jpg", "1.png", "2.jpg"] 
 
 # you need your song in both mp3 and wav format
-your_song_wav = "Undertale- Megalovania.wav"
-your_song_mp2 = "Undertale- Megalovania.mp3"
+your_song_wav = "Zack Hemsey - The Hands Of An Ape [High quality].wav"
+your_song_mp2 = "Zack Hemsey - The Hands Of An Ape.mp3"
 
 # the max number of picture's swaps in 1 second
-movement_rate = 25
+movement_rate = 15
 
 #a numpy array with the moments where beats happens 
-beats = beat_gen(your_song_wav , 25)
+beats = beat_gen(your_song_wav , movement_rate)
 
 #a list with the generated synchronized sequences 
 clips = clips_gen(beats, img)
